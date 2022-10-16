075cadeb8f485298eebe4d786f62f15d756107e3
Zachary Morden <78518056+SetKu@users.noreply.github.com>
2022-10-16 14:22:22 -0600

    Added initial data structures.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+// No defined size.
+#[derive(Debug)]
+struct Superposition {
+    candidates: Vec<Tile>
+}
+
+impl Superposition {
+    fn is_collapsed(&self) -> bool {
+        return self.candidates.len() == 1;
+    }
+}
+
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+    UpRight,
+    DownRight,
+    UpLeft,
+    DownLeft,
+}
+
+#[derive(Debug)]
+struct Tile {
+    contradictions: Vec<(Box<Tile>, Box<Tile>, Direction)>,
+    weight: f32,
+}
+
+#[derive(Debug)]
+struct Coordinator {
+    superpositions: Vec<Box<Superposition>>
+}
+
+impl Coordinator {
+    fn new() -> Self {
+        Coordinator { superpositions: vec![] }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut coord = Coordinator::new();
 }
