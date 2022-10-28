05d3ca4fe18a3a783ea04d1f5e39b7f6561187de
SetKu <78518056+SetKu@users.noreply.github.com>
2022-10-27 22:56:14 -0600

    Cleaned up main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 // Modules in Rust: https://is.gd/gRhcqA
 // Modules Cheat Sheet: https://is.gd/WusVq8
 // Cross Compilation Possibility: https://kerkour.com/rust-cross-compilation
-use wave_func_collapse::location::*;
-use std::collections::hash_map;
-use bitset_core::BitSet;
+use wave_func_collapse::Collapser;
 
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
-
     let sample: String = include_str!("sample.txt").to_string().replace(", ", "");
 }
