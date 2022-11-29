dfcbd89679abe4f721c9437550596df54006a15c
SetKu <78518056+SetKu@users.noreply.github.com>
2022-11-29 10:06:38 -0700

    Added colours and time breakdown.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use cgmath::Vector2;
 use clap::{arg, crate_version, value_parser, Arg, Command};
 use std::fs;
 use std::path::PathBuf;
+use std::time::Instant;
 use wfc::{helpers::xy_swap, helpers::dimensions_of, BorderMode, Flags, Wave};
 
 fn main() -> Result<(), String> {
@@ -31,10 +32,10 @@ fn main() -> Result<(), String> {
         .arg(arg!( -c --contradictions <number> "The maximum number of contradictions (attempts) that can be reached before the program panics.")
             .value_parser(value_parser!(usize)))
         .arg(arg!( -p --noprint "(Fast Mode 🚀) Disables incrementally printing the function's progress. This also removes artificially induced delays for human readability."))
-        .arg(arg!( -q --quiet "Makes the output suitable for analysis by other programs, removing the commas and label text. This option also invokes the 'p' flag."))
         .arg(arg!( -w --noweights "Disables using weights in the randomization process."))
         .arg(arg!( -t --notransforms "Disables using transforms in rule analysis."))
         .arg(arg!( -l --usewhitespace "Takes into account whitespace in the sample."))
+        .arg(arg!( -d --disablecom "Disables stripping commas from the input sample."))
         .get_matches();
 
     let width = *matches.get_one::<usize>("width").unwrap();
@@ -43,27 +44,34 @@ fn main() -> Result<(), String> {
     let tilesize_width = matches.get_one::<usize>("tilesizewidth");
     let tilesize_height = matches.get_one::<usize>("tilesizeheight");
     let pathbuf = matches.get_one::<PathBuf>("sample");
-    let mut print = !matches.get_flag("noprint");
+    let print = !matches.get_flag("noprint");
     let use_weights = !matches.get_flag("noweights");
     let use_transforms = !matches.get_flag("notransforms");
-    let simple_output = matches.get_flag("quiet");
     let max_contradictions = matches.get_one::<usize>("contradictions");
     let use_whitespace = matches.get_flag("usewhitespace");
-
-    if simple_output {
-        print = false;
-    }
+    let disablecommas = matches.get_flag("disablecom");
 
     let input: String = if let Some(buf) = pathbuf {
-        fs::read_to_string(buf)
-            .expect("The sample provided cannot be read or is invalid")
-            .replace(", ", "")
-            .replace(",", "")
+        let content = fs::read_to_string(buf)
+            .expect("The sample provided cannot be read or is invalid");
+
+        if !disablecommas {
+            content
+                .replace(", ", "")
+                .replace(",", "")
+        } else {
+            content
+        }
     } else {
-        include_str!("sample.txt")
-            .to_string()
-            .replace(", ", "")
-            .replace(",", "")
+        let content = include_str!("sample.txt").to_string();
+
+        if !disablecommas {
+            content
+                .replace(", ", "")
+                .replace(",", "")
+        } else {
+            content
+        }
     };
 
     if input.is_empty() {
@@ -103,7 +111,9 @@ fn main() -> Result<(), String> {
         wave.flags.push(Flags::NoWeights);
     }
 
+    let a_start = Instant::now();
     wave.analyze(sample, chunk_size, BorderMode::Clamp);
+    let a_dur = a_start.elapsed();
     wave.fill(Vector2::new(width, height))?;
 
     let real_contradictions = if let Some(max) = max_contradictions {
@@ -112,6 +122,7 @@ fn main() -> Result<(), String> {
         max_contradictions_default
     };
 
+    let c_start = Instant::now();
     wave.collapse_all(
         real_contradictions,
         if print {
@@ -121,10 +132,17 @@ fn main() -> Result<(), String> {
             })
         } else { None },
     )?;
+    let c_dur = c_start.elapsed();
 
     let result = wave.perfect_rep()?;
-    let string = reconstruct_string(result, &source_map);
+    let use_colors = if print { pathbuf.is_none() } else { false };
+    let string = reconstruct_string(result, &source_map, use_colors, use_colors);
     println!("{}", string);
+
+    if print {
+        println!("\nAnalysis Time: {:?}", a_dur);
+        println!("Collapse Time: {:?}", c_dur);
+    }
 
     Ok(())
 }
@@ -212,9 +230,14 @@ fn construct_wip_string(input: Vec<Vec<Vec<usize>>>, source_map: &Vec<(usize, ch
     output
 }
 
-fn reconstruct_string(input: Vec<Vec<usize>>, source_map: &Vec<(usize, char)>) -> String {
+fn reconstruct_string(input: Vec<Vec<usize>>, source_map: &Vec<(usize, char)>, use_color: bool, bold: bool) -> String {
     let swapped = xy_swap(input);
     let mut output = "".to_string();
+
+    if bold {
+        output.push_str("\x1b[1m");
+    }
+
     let mut lines = 1;
 
     for (r, row) in swapped.iter().enumerate() {
@@ -225,8 +248,28 @@ fn reconstruct_string(input: Vec<Vec<usize>>, source_map: &Vec<(usize, char)>) -
 
         for id in row {
             let real_val = source_map.iter().find(|s| s.0 == *id).unwrap().1;
+
+            if use_color {
+                output.push_str("\x1b[");
+
+                if real_val == 'S' {
+                    output.push_str("34m");
+                } else if real_val == 'C' {
+                    output.push_str("33m");
+                } else if real_val == 'L' {
+                    output.push_str("32m");
+                } else {
+                    output.push_str("35m");
+                }
+            }
+
             output.push_str(&format!("{}, ", real_val));
         }
+    }
+
+    if bold || use_color {
+        // reset terminal style
+        output.push_str("\x1b[0m");
     }
 
     output
