2e5b3c63786d7f66f438d647841a58d6aae86fa1
Zachary Morden <78518056+SetKu@users.noreply.github.com>
2023-01-21 23:23:54 -0700

    Async attempts and borrow checker complaints...

--- wavefc-app/src/main.rs
+++ wavefc-app/src/main.rs
@@ -1,4 +1,11 @@
-use eframe::{egui, epaint::{vec2, Color32}};
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use eframe::{
+    egui,
+    epaint::{vec2, Color32, ColorImage},
+};
+use egui_extras::RetainedImage;
+use std::{path::PathBuf, thread, sync::{Mutex, Arc}};
 
 const APP_NAME: &str = "Wave Function Collapse";
 
@@ -9,34 +16,96 @@ fn main() {
         ..Default::default()
     };
 
-    eframe::run_native(APP_NAME, options, Box::new(|_cc| Box::new(WFCApp::default())));  
+    eframe::run_native(
+        APP_NAME,
+        options,
+        Box::new(|_cc| Box::new(WFCApp::default())),
+    );
 }
 
 struct WFCApp {
-    dropped_files: Vec<egui::DroppedFile>,
-    image_source_path: Option<String>,
+    image_source_path: Option<PathBuf>,
+    image_retainer: Arc<Mutex<Option<RetainedImage>>>,
 }
 
 impl Default for WFCApp {
     fn default() -> Self {
         Self {
-            dropped_files: vec![],
             image_source_path: None,
+            image_retainer: Arc::new(Mutex::new(None)),
         }
     }
 }
 
+impl WFCApp {
+    fn background_update_retainer<'a>(&'static self, path: PathBuf) {
+        thread::spawn(move || {
+            let retainer = updated_image_retainer(path);
+            
+            if let Ok(mut reference) = self.image_retainer.lock() {
+                *reference = retainer;
+            }
+        });
+    }
+}
+
 impl eframe::App for WFCApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            // Increases global UI scale but causes
+            // half-second blank screen on resize and start.
+            // ctx.set_pixels_per_point(2.);
+
             ui.visuals_mut().override_text_color = Some(Color32::WHITE);
             ui.heading(egui::RichText::new("Wave Function Collapse"));
             ui.label("By SetKu");
             ui.add_space(10.);
 
             if ui.button("Import Source Image").clicked() {
-                println!("clicked");
+                if let Some(path) = rfd::FileDialog::new().pick_file() {
+                    self.image_source_path = Some(path.clone());
+                    self.background_update_retainer(path);
+                }
+            }
+
+            ui.label("You can also drag and drop the image into the window.");
+
+            if !ctx.input().raw.dropped_files.is_empty() {
+                let first = ctx.input().raw.dropped_files.first().unwrap().clone();
+                self.image_source_path = first.path;
+
+                // thread::spawn(|| {
+                //     let retainer = updated_image_retainer(path);
+                // });
+            }
+
+            if let Some(path) = &self.image_source_path {
+                ui.add_space(5.);
+                ui.label(format!("Selected Image Source: {}", path.display()));
+
+                if let Ok(ret_lock) = self.image_retainer.try_lock() {
+                    if let Some(ret) = ret_lock.as_ref() {
+                        ret.show_size(ui, vec2(100., 100.));
+                    }
+                }
             }
         });
     }
+}
+
+fn updated_image_retainer(path: PathBuf) -> Option<RetainedImage> {
+    if let Ok(image) = image::io::Reader::open(path) {
+        if let Ok(decoded) = image.decode() {
+            let data = decoded.to_rgba8();
+            let pixels = data.as_flat_samples().samples;
+            let paint_image = ColorImage::from_rgba_unmultiplied(
+                [decoded.width() as _, decoded.height() as _],
+                pixels,
+            );
+                
+            return Some(RetainedImage::from_color_image("chosen-image", paint_image));
+        }
+    }
+
+    return None;
 }
