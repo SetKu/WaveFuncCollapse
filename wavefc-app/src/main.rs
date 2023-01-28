9dcdfc9e2923ce9091cae8b67506ca0285ea2cad
Zachary Morden <78518056+SetKu@users.noreply.github.com>
2023-01-27 23:59:51 -0700

    Additional UI progress.

--- wavefc-app/src/main.rs
+++ wavefc-app/src/main.rs
@@ -2,8 +2,8 @@
 
 use eframe::{
     egui,
-    egui::{Frame, Button, WidgetText, FontDefinitions},
-    epaint::{vec2, Color32, ColorImage, Stroke},
+    egui::{Button, FontDefinitions, Frame, WidgetText, Spinner},
+    epaint::{vec2, Color32, ColorImage, Rect, Rounding, Stroke, Vec2},
 };
 use egui_extras::RetainedImage;
 use std::{
@@ -11,6 +11,7 @@ use std::{
     sync::{Arc, Mutex},
     thread,
 };
+use wavefc::prelude::*;
 
 const APP_NAME: &str = "Wave Function Collapse";
 
@@ -52,7 +53,10 @@ impl WFCApp {
         *self.loading_image.lock().unwrap() = true;
 
         thread::spawn(move || {
+            let started = std::time::Instant::now();
+            println!("STARTED LOADING IMAGE");
             let retainer = updated_image_retainer(path);
+            println!("OBTAINED IMAGE ({:?}): {:?}", started.elapsed(), retainer.is_some());
 
             if let Ok(mut reference) = i_arc_copy.lock() {
                 *reference = retainer;
@@ -68,7 +72,7 @@ impl WFCApp {
 impl eframe::App for WFCApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default()
-            .frame(Frame::none().fill(Color32::WHITE).inner_margin(12.))
+            .frame(Frame::none().fill(Color32::WHITE).inner_margin(10.))
             .show(ctx, |ui| {
                 // Increases global UI scale but causes
                 // half-second blank screen on resize and start.
@@ -77,6 +81,7 @@ impl eframe::App for WFCApp {
                 ui.visuals_mut().override_text_color = Some(Color32::BLACK);
 
                 let width = ui.available_width();
+                let height = ui.available_height();
 
                 ui.horizontal_top(|ui| {
                     ui.vertical(|ui| {
@@ -87,7 +92,10 @@ impl eframe::App for WFCApp {
                         ui.label("By SetKu");
                         ui.add_space(10.);
 
-                        let button = Button::new(WidgetText::from("Import Source Image").color(Color32::WHITE));
+                        let button = Button::new(
+                            WidgetText::from("Import Source Image").color(Color32::WHITE),
+                        );
+
                         if ui.add(button).clicked() {
                             if let Some(path) = rfd::FileDialog::new().pick_file() {
                                 self.image_path = Some(path.clone());
@@ -100,24 +108,50 @@ impl eframe::App for WFCApp {
 
                         if let Some(path) = &self.image_path {
                             ui.label(format!("Selected Image Source: {}", path.display()));
+                            ui.add_space(10.);
 
                             if *self.loading_image.lock().unwrap() {
-                                ui.label("Actively loading image...");
+                                ui.label("Loading image...");
+                                ui.add(Spinner::default().size(20.).color(Color32::BLACK));
                             } else {
                                 if let Ok(ret_lock) = self.image_retainer.try_lock() {
                                     if let Some(ret) = ret_lock.as_ref() {
-                                        ret.show_max_size(ui, vec2(300., 300.));
+                                        ret.show_max_size(ui, vec2(width / 2., height / 2.));
                                     }
                                 }
                             }
                         }
                     });
 
-                    ui.separator();
+                    // ui.separator();
 
                     ui.vertical(|ui| {
                         ui.set_min_width(width / 2.);
                         ui.set_max_width(width / 2.);
+                        let origin = ui.next_widget_position();
+
+                        // Grid Background
+                        ui.painter().rect_filled(
+                            Rect::from_min_size(origin, Vec2::new(width / 2., height)),
+                            Rounding::none(),
+                            Color32::DARK_BLUE,
+                        );
+
+                        // Grid Square Template
+                        const HORIZONTAL_TILES: u32 = 8;
+
+                        ui.painter_at(Rect::from_min_size(origin, Vec2::new(width / 4., height)))
+                            .rect_filled(
+                                Rect::from_min_size(
+                                    origin,
+                                    Vec2::new(
+                                        width / 2. / HORIZONTAL_TILES as f32,
+                                        width / 2. / HORIZONTAL_TILES as f32,
+                                    ),
+                                ),
+                                Rounding::none(),
+                                Color32::GREEN,
+                            );
                     });
                 });
             });
@@ -136,12 +170,14 @@ impl eframe::App for WFCApp {
 fn updated_image_retainer(path: PathBuf) -> Option<RetainedImage> {
     if let Ok(image) = image::io::Reader::open(path) {
         if let Ok(decoded) = image.decode() {
+            println!("DECODED IMAGE SUCCESSFULLY");
             let data = decoded.to_rgba8();
             let pixels = data.as_flat_samples().samples;
             let paint_image = ColorImage::from_rgba_unmultiplied(
                 [decoded.width() as _, decoded.height() as _],
                 pixels,
             );
+            println!("FINISHED INTERMEDIATE PROCESSING");
 
             return Some(RetainedImage::from_color_image("chosen-image", paint_image));
         }
