d9f29a20eb07f688790206ef1d851eb1f3fa78ef
SetKu <78518056+SetKu@users.noreply.github.com>
2023-02-09 22:48:23 -0700

    Finished tests and string deconstructor.

--- wavefc/src/helpers/mod.rs
+++ wavefc/src/helpers/mod.rs
@@ -1,17 +1,15 @@
-#![warn(
-    clippy::pedantic,
-    clippy::nursery,
-)]
+#![warn(clippy::pedantic, clippy::nursery)]
 
 #[cfg(test)]
 mod tests;
 
 use cgmath::{vec2, Vector2, Zero};
+use std::collections::HashMap;
 
 /// This function finds the width and height of the given 2D array.
 ///
 /// Assumes constant uniform internal vector length.
-pub(crate) fn dimensions_of<T>(input: &[Vec<T>]) -> Vector2<usize> {
+pub fn dimensions_of<T>(input: &[Vec<T>]) -> Vector2<usize> {
     Vector2::new(
         input.len(),
         if input.is_empty() { 0 } else { input[0].len() },
@@ -19,14 +17,14 @@ pub(crate) fn dimensions_of<T>(input: &[Vec<T>]) -> Vector2<usize> {
 }
 
 #[derive(Debug)]
-pub(crate) struct Adjacency {
-    pub(crate) root_data: Vec<Vec<u32>>,
+pub struct Adjacency {
+    pub root_data: Vec<Vec<u32>>,
     // Array holds values for top (0), right (1), bottom (2), and left (3).
-    pub(crate) neighbours_data: [Option<Vec<Vec<u32>>>; 4],
+    pub neighbours_data: [Option<Vec<Vec<u32>>>; 4],
 }
 
 // Overlapping
-pub(crate) fn adjacencies(
+pub fn adjacencies(
     input: &[Vec<u32>],
     chunk_size: Vector2<u32>,
     up_is_greater: bool,
@@ -39,8 +37,8 @@ pub(crate) fn adjacencies(
     let mut list: Vec<Adjacency> = vec![];
 
     // Avoid calculating for a root that doesn't exist.
-    for x in 0..(size.x - chunk_size.x as usize + 1) {
-        for y in 0..(size.y - chunk_size.y as usize + 1) {
+    for x in 0..=(size.x - chunk_size.x as usize) {
+        for y in 0..=(size.y - chunk_size.y as usize) {
             let mut root = vec![]; // [x][y]
 
             for ix in 0..(chunk_size.x as usize) {
@@ -119,7 +117,7 @@ pub(crate) fn adjacencies(
 }
 
 /// Returns in the order top (0), right, bottom, left. Assumes top has an incremented y-value.
-pub(crate) fn orthogonal(o: &Vector2<i32>) -> [Vector2<i32>; 4] {
+pub const fn orthogonal(o: Vector2<i32>) -> [Vector2<i32>; 4] {
     [
         Vector2::new(o.x, o.y + 1),
         Vector2::new(o.x + 1, o.y),
@@ -128,11 +126,57 @@ pub(crate) fn orthogonal(o: &Vector2<i32>) -> [Vector2<i32>; 4] {
     ]
 }
 
-pub(crate) fn remove_indexes<T>(vec: &mut Vec<T>, indexes: Vec<usize>) {
-    let mut removed = 0_usize;
-
-    for i in indexes {
+pub fn remove_indexes<T>(vec: &mut Vec<T>, indexes: &[usize]) {
+    for (removed, i) in indexes.iter().enumerate() {
         vec.remove(i - removed);
-        removed += 1;
     }
+}
+
+#[derive(Debug)]
+pub struct Sample<T> {
+    grid: Vec<Vec<u32>>,
+    map: HashMap<u32, T>,
+}
+
+pub fn deconstruct_string(input: &str, use_whitespace: bool) -> Sample<char> {
+    let mut sample = Sample {
+        grid: Vec::with_capacity(input.lines().count()),
+        map: HashMap::new(),
+    };
+
+    let mut id_counter = 0;
+
+    for (y, text) in input.lines().enumerate() {
+        let chars = text.chars();
+
+        for (x, ch) in if use_whitespace {
+            chars.enumerate().collect::<Vec<(usize, char)>>()
+        } else {
+            chars.filter(|v| !v.is_whitespace()).enumerate().collect()
+        } {
+            dbg!(x);
+
+            if sample.grid.len() < x + 1 {
+                // Make space for at least the current y-value.
+                // Saves time.
+                sample.grid.push(Vec::with_capacity(y));
+                println!("added new col");
+            }
+
+            let translation: u32 = sample
+                .map
+                .iter()
+                .find_map(|(k, v)| if *v == ch { Some(*k) } else { None })
+                .unwrap_or_else(|| {
+                    let new_id = id_counter;
+                    sample.map.insert(new_id, ch);
+                    id_counter += 1;
+                    new_id
+                });
+
+            sample.grid[x].push(translation);
+        }
+    }
+
+    sample
 }
--- wavefc/src/helpers/tests.rs
+++ wavefc/src/helpers/tests.rs
@@ -2,7 +2,7 @@ use super::*;
 
 #[test]
 fn orthogonals_works() {
-    let neighbours = orthogonal(&Vector2::new(4, 4));
+    let neighbours = orthogonal(Vector2::new(4, 4));
     assert_eq!(neighbours[0], Vector2::new(4, 5));
     assert_eq!(neighbours[1], Vector2::new(5, 4));
     assert_eq!(neighbours[2], Vector2::new(4, 3));
@@ -13,6 +13,37 @@ fn orthogonals_works() {
 fn remove_indexes_works() {
     let mut input = vec![5, 10, 15, 20, 25, 30];
     let to_remove = vec![0, 2, 4];
-    remove_indexes(&mut input, to_remove);
+    remove_indexes(&mut input, &to_remove);
     assert_eq!(input, vec![10, 20, 30]);
+}
+
+#[test]
+fn string_deconstructor_works() {
+    let input = "S C L\nC L L\nL L L";
+    let result = deconstruct_string(input, false).grid;
+
+    assert_eq!(
+        result,
+        vec![vec![0, 1, 2,], vec![1, 2, 2,], vec![2, 2, 2,],]
+    );
+}
+
+#[test]
+fn adjacencies_works() {
+    let input = vec![vec![0, 1, 2], vec![1, 2, 2], vec![2, 2, 2]];
+
+    let simple_test = adjacencies(&input, vec2(1, 1), true);
+    assert_eq!(simple_test.len(), 9);
+
+    let complex_test = adjacencies(&input, vec2(1, 2), true);
+    assert_eq!(complex_test.len(), 6);
+    assert_eq!(
+        complex_test
+            .iter()
+            .filter(|v| v.root_data == vec![vec![2, 2]]
+                && v.neighbours_data
+                    == [None, Some(vec![vec![2, 2]]), None, Some(vec![vec![1, 2]]),])
+            .count(),
+        1
+    );
 }
