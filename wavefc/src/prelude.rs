1c7f44183c836e67eafd1b044041506d08e8ac79
SetKu (Zachary Morden) <78518056+SetKu@users.noreply.github.com>
2025-12-03 15:12:15 -0700

    feat!: scrubbed entire wavefc crate; starting from scratch
    - Migrated to 2024 edition.
    
    BREAKING CHANGE: we will now be developing an entirely new API from here
    on in.

--- wavefc/src/prelude.rs
+++
@@ -1,8 +0,0 @@
-#![allow(unused_imports)]
-
-pub use crate::helpers::dimensions_of;
-pub use crate::stringtools::*;
-pub use crate::BorderMode;
-pub use crate::Flags;
-pub use crate::Wave;
-pub use cgmath::Vector2;
