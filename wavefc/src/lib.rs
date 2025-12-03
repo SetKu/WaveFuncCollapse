980e81be17efb4c1a735768f85f16fa7fbeb0d73
SetKu (Zachary Morden) <78518056+SetKu@users.noreply.github.com>
2025-12-03 15:36:53 -0700

    feat: rudimentary types

--- wavefc/src/lib.rs
+++ wavefc/src/lib.rs
@@ -1,5 +1,16 @@
 #[derive(Clone)]
-struct Wave {
+struct Wave<C> {
+    cells: Vec<C>,
+}
 
+#[derive(PartialEq, Eq)]
+enum Relation {
+    CanBeAdjacentTo,
+    ConsistsOf,
+}
+
+#[derive(PartialEq, Eq)]
+struct Constraint {
+    kind: Relation,
 }
 
